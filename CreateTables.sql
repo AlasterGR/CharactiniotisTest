02c3103236b5e3cab8d217dbca6452ed9fed5c6d
Alaster <alexdab@windowslive.com>
2024-04-24 16:42:53 +0300

    Add files via upload

--- CreateTables.sql
+++ CreateTables.sql
@@ -4,12 +4,17 @@ CREATE TABLE IF NOT EXISTS Clients (
     FirstName VARCHAR(50) NOT NULL,
     LastName VARCHAR(50) NOT NULL,
     Address VARCHAR(100) NOT NULL,
-    PostalCode NUMERIC(5,0) NOT NULL, -- I use NUMERIC data type to store exactly 5 digits
+    PostalCode NUMERIC(5,0) NOT NULL,
     PhoneNumber NUMERIC(10, 0) NOT NULL UNIQUE, -- I will not provide for the country code
     Email VARCHAR(320) UNIQUE,
     CONSTRAINT PostalCode_length_check CHECK (LENGTH(PostalCode::TEXT) = 5), -- Costraint PostalCode to exactly 5 digits,
     CONSTRAINT PhoneNumber_length_check CHECK (LENGTH(PhoneNumber::TEXT) = 10) -- Costraint PhoneNumber to exactly 10 digits
 );
+
+-- Create unique index on Email column allowing multiple empty entries
+CREATE UNIQUE INDEX IF NOT EXISTS idx_unique_email_non_empty
+ON Clients (Email)
+WHERE Email IS NOT NULL;
 
 -- Table for Books
 CREATE TABLE IF NOT EXISTS Books (
@@ -24,16 +29,16 @@ CREATE TABLE IF NOT EXISTS Books (
 CREATE TABLE IF NOT EXISTS OrderHeader (
     OrderID SERIAL PRIMARY KEY,
     ClientID INT NOT NULL,
-    OrderDate DATE NOT NULL,
+    --OrderDate DATE NOT NULL,
     FOREIGN KEY (ClientID) REFERENCES Clients(ClientID)
 );
 
 -- Table for Order Details
 CREATE TABLE IF NOT EXISTS OrderDetails (
     --OrderDetailID SERIAL PRIMARY KEY,
-    OrderID INT NOT NULL UNIQUE,
+    OrderID INT NOT NULL,
     ISBN NUMERIC(13,0) NOT NULL UNIQUE, -- UNIQUE because we have already presumed to have 1` of any book
-    Quantity INT NOT NULL,
+    --Quantity INT NOT NULL,
     FOREIGN KEY (OrderID) REFERENCES OrderHeader(OrderID),
     FOREIGN KEY (ISBN) REFERENCES Books(ISBN)
 );
